88a9ff62106a9d155881a7457040cd0aac5d36f3
Sagar Jadhav <sagarj.jadhav23@gmail.com>
2018-10-28 19:45:01 +0530

    Removing outdated code

--- dockerfile
+++
@@ -1,9 +0,0 @@
-FROM java:8
-
-ADD /build/libs/meetup-0.0.1-SNAPSHOT.jar /meetup-0.0.1-SNAPSHOT.jar
-
-WORKDIR /
-
-EXPOSE 8080
-
-ENTRYPOINT ["java","-jar","meetup-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/sjtech/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sjtech.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sjtech.domain.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-
-}
